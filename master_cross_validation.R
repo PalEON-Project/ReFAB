e434c23b7a64bf81dd0e7e39e0c75e2a745aca99
ANN RAIHO <araiho@nd.edu>
2017-07-28 10:14:02 -0400

    simplifying cross validation scripts

--- master_cross_validation.R
+++ master_cross_validation.R
@@ -16,58 +16,13 @@ if(!file.exists('allPredData.Rda'))
 load('allPredData.Rda')
 
 source('~/ReFAB/genPareto/model_dgp_auxil.R')  # BUGS code for model
-source('~/ReFAB/genPareto/fit_fix_sigma.R')        # contains fit() function
+source('~/ReFAB/fit_fix_sigma.R')        # contains fit() function
 
 #adding settlement biomass to the metadata matrix in the most annoying way possible
 ##TO DO: put somewhere else
 library(fields)
-biomass_dat_est <- read.csv(paste0(data.dir,"biomass_prediction_v0.9-7_bam.csv"))
-
-##### Changing pollen coordinates so that we can find the right rows when we find the biomass for each pond.
-lat.long.reg <- cbind(as.numeric(as.character(x.meta$long)),as.numeric(as.character(x.meta$lat)))
-lat.long.reg.df = data.frame(lat.long.reg)
-colnames(lat.long.reg.df) = c('x', 'y')
-
-coordinates(lat.long.reg.df) <- ~ x + y
-proj4string(lat.long.reg.df) <- CRS('+proj=longlat +ellps=WGS84')
-
-albers <- spTransform(lat.long.reg.df, CRS('+init=epsg:3175'))
-albers <- as.matrix(data.frame(albers))
-
-centers_biomass = cbind(biomass_dat_est$x,biomass_dat_est$y)
-idx_cores = vector(length=nrow(x.meta))
-
-for(i in 1:nrow(x.meta)){   
-  core_site = albers[i,]
-  d = rdist(matrix(core_site, ncol=2), as.matrix(centers_biomass))
-  if(min(d)<8000){
-    idx_cores[i] = which.min(d) 
-  }else{
-    idx_cores[i] = NA 
-  }
-  
-}
-
-biomass <- list()
-for(i in 1:nrow(x.meta)){ 
-  biomass[[i]] = biomass_dat_est[idx_cores[i],'Total']
-}
-
-x.meta <- cbind(x.meta,unlist(biomass))
-colnames(x.meta)[ncol(x.meta)]<-c('SettleBiomass')
-
-#stewerts dark\
-#lake O' Pines
 
 locn <- 'Cub Lake'
-site_number = unique(x.meta[x.meta$site.name == locn,1])
-ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
-
-Y = as.matrix(ten_count_use)
-
-sigma.values <- c(.01, .03, .09, .27, .81)
-
-# pull out data for locn of interest
 site_number = unique(x.meta[x.meta$site.name == locn,1])
 ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
 
--- write.cross.validation.scripts.R
+++ write.cross.validation.scripts.R
@@ -1,39 +1,23 @@
 
-# load in data for all sites, per Ann's original code
-if(!file.exists('allPredData.Rda'))
-  source('prep_data.R') 
-load('allPredData.Rda')
+sigma.vals <- c(.01,.03,.09,.27,.81)
 
-site.names <- unique(x.meta$site.name)
-how.many <- list()
 
-for(i in 1:length(site.names)){ #44:length(site.names)
-  locn <- site.names[i]
-  site_number = unique(x.meta[x.meta$site.name == locn,1])
-  ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
-  
-  Y = as.matrix(ten_count_use)
-  if(length(Y)>21 & nrow(Y) > 15 &
-     max(x.meta[x.meta$site.name == locn,'age_bacon'])>8000 & 
-     min(x.meta[x.meta$site.name == locn,'age_bacon'])<2000){
+for(s in seq_along(sigma.vals)){
+  for(g in 1:10){
+    master.test <- readLines('~/ReFAB/master_cross_validation.R')
+    master.test <- gsub("SIGMA", sigma.vals[s], master.test)
+    master.test <- gsub("GROUP", g, master.test)
+    locnClean <- gsub(' ', '-', SITE)
+    locnClean <- gsub("'", '-', locnClean)
+    writeLines(master.test, con=paste0('master.',locnClean,g,sigma.vals[s],'.R'))
     
-    how.many[[i]]<- locn
+    jobsh <- readLines('~/ReFAB/template.job.sh')
+    jobsh <- gsub('@SITE@',paste0(locnClean,g,sigma.vals[s]),jobsh)
+    writeLines(jobsh, con=paste0(locnClean,g,sigma.vals[s],'.sh'))
+    
+    system(paste('qsub',paste0(locnClean,g,sigma.vals[s],'.sh')))
   }
+
 }
 
-names(how.many) <- site.names[1:177]
-how.many <- unlist(how.many)
 
-for(i in 1:length(how.many)){
-  SITE <- names(how.many)[i]
-  
-  master.test <- readLines('~/ReFAB/genPareto/master.R')
-  master.test <- gsub("SITE", SITE, master.test)
-  locnClean <- gsub(' ', '-', SITE)
-  locnClean <- gsub("'", '-', locnClean)
-  writeLines(master.test, con=paste0('master.',locnClean,'.R'))
-  
-  jobsh <- readLines('~/ReFAB/template.job.sh')
-  jobsh <- gsub('@SITE@',locnClean,jobsh)
-  writeLines(jobsh, con=paste0(locnClean,'.sh'))
-}
