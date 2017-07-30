23db0d951f6299e03f767d66f26678c09d470ee8
ANN RAIHO <araiho@nd.edu>
2017-07-30 13:26:30 -0400

    fixing names and adding more sites for job submission and adding geo buy flag

--- template.job.sh
+++ template.job.sh
@@ -3,6 +3,7 @@
 #$ -l h_rt=24:00:00
 #$ -pe smp 1
 #$ -N @SITE@.raiho
+#$ -q "geo*"
 
 module load R/3.4.0
 
--- write.cross.validation.scripts.R
+++ write.cross.validation.scripts.R
@@ -1,6 +1,34 @@
 
 sigma.vals <- c(.01,.03,.09,.27,.81)
-site <- 'Wintergreen Lake'
+
+# load in data for all sites, per Ann's original code
+if(!file.exists('allPredData.Rda'))
+  source('prep_data.R') 
+load('allPredData.Rda')
+
+site.names <- unique(x.meta$site.name)
+how.many <- list()
+
+for(i in 1:length(site.names)){ #44:length(site.names)
+  locn <- site.names[i]
+  site_number = unique(x.meta[x.meta$site.name == locn,1])
+  ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
+  
+  Y = as.matrix(ten_count_use)
+  if(length(Y)>21 & nrow(Y) > 15 &
+     max(x.meta[x.meta$site.name == locn,'age_bacon'])>8000 & 
+     min(x.meta[x.meta$site.name == locn,'age_bacon'])<2000){
+    
+    how.many[[i]]<- locn
+  }
+}
+
+names(how.many) <- site.names[1:177]
+how.many <- unlist(how.many)
+
+for(i in 1:length(how.many)){
+  site <- names(how.many)[i]
+  
 
 for(s in seq_along(sigma.vals)){
   for(g in 1:10){
@@ -21,4 +49,4 @@ for(s in seq_along(sigma.vals)){
 
 }
 
-
+}
