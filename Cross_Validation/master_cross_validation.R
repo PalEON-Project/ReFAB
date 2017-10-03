c2fd736cc35c864401a3ab2ed55a164d774ce43a
ANN RAIHO <araiho@nd.edu>
2017-10-03 13:42:45 -0400

    fixing cross validation dir error

--- Cross_Validation/master_cross_validation.R
+++ Cross_Validation/master_cross_validation.R
@@ -11,7 +11,7 @@ if (is.na(arg[1])) {
   runnum <- as.numeric(arg[1])
 }
 
-dataID <- read.csv(file.path(crossValDir,'dataID.csv'))
+dataID <- read.csv(file.path('Cross_Validation','dataID.csv'))
 
 ####
 #### Master Setup
