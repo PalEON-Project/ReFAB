5280f606b738b23a2a72bffedc017f212699c752
ANN RAIHO <araiho@nd.edu>
2017-11-16 08:47:12 -0500

    Adding layers to show different ecosystems for paper

--- Development_Code/xiao_biomass_map_code.R
+++ Development_Code/xiao_biomass_map_code.R
@@ -18,6 +18,7 @@ library(rjags)
 
 biomass_dat_est <- read.csv(paste0("~/Downloads/","biomass_prediction_v0.9-10_bam.csv"))
 xiao_ests <- biomass_dat_est$Total#rowSums(biomass_dat_est[,4:23])
+hem.beech <- rowSums(biomass_dat_est[,c('Hemlock','Beech')])
 
 usShp <- readShapeLines(file.path("/Users/paleolab/Documents/babySTEPPS/", 'us_alb.shp'), proj4string=CRS('+init=epsg:3175'))
 usShp@data$id <- rownames(usShp@data)
@@ -43,30 +44,74 @@ theme_clean <- function(plot_obj){
   return(plot_obj)
 }
 
-full.mat <- cbind(biomass_dat_est[,c('x','y')],xiao_ests)
-colnames(full.mat) <- c("x","y","Xiao Total Biomass")
-y = as.data.frame(full.mat)
+### Biomass
+full.mat <- cbind(biomass_dat_est[,c('x','y')],xiao_ests,hem.beech)
+colnames(full.mat) <- c("x","y","Tot_Biomass",'late_succ')
+tot.biom.df = as.data.frame(full.mat)
+
+### Density
+density <- read.csv('~/Downloads/plss_density_alb_v0.9-10.csv')
+density.df = data.frame(x = density$x, y = density$y, 
+           density = rowSums(density[,4:ncol(density)]))
+
+y <- merge(tot.biom.df,density.df,by=c('x','y'))
 
 breaks <-  c(seq(0,50,10),seq(75,200,25))
 colors <- rev(terrain.colors(length(breaks)-1))
 
+density.breaks <- c(0,1,48,1300)
+
 legendName <- "Biomass (Mg/ha)"
 
-data_binned <-  cut(y[,3], breaks, include.lowest = TRUE, labels = FALSE)
+data_binned <-  cut(y$Tot_Biomass, breaks, include.lowest = TRUE, labels = FALSE)
+data_binned_density <-  cut(y$density, density.breaks, include.lowest = TRUE, labels = FALSE)
 
-breaklabels <- apply(cbind(breaks[1:(length(breaks)-1)], breaks[2:length(breaks)]), 1,  function(r) { sprintf("%0.2f - %0.2f", r[1], r[2]) })
+breaklabels <- apply(cbind(breaks[1:(length(breaks)-1)],
+                           breaks[2:length(breaks)]), 1,
+                     function(r) { sprintf("%0.2f - %0.2f", 
+                                           r[1], r[2]) })
 
-inputData <- data.frame(X = y[,1], Y = y[,2], Preds = cbind(data_binned,data_binned))
+inputData <- data.frame(X = y$x, Y = y$y,
+                        Preds = data_binned,tot_biomass = y$Tot_Biomass,
+                        density = y$density, LS = y$late_succ)
 inputData_long <- melt(inputData, c('X', 'Y'))
 
+load('calibration.albers.rdata')
 input_points <- data.frame(albers) # how to add points part A
 colnames(input_points) <- c('lat','lon')
 
-d <- ggplot() + geom_raster(data = inputData_long, aes(x = X, y = Y, fill = factor(value))) + scale_fill_manual(labels = breaklabels, name = legendName, drop = FALSE, values = colors, guide = "legend") + 
-  theme(strip.text.x = element_text(size = 16), legend.text = element_text(size = 16), legend.title = element_text(size = 16)) +
-  geom_point(data = input_points, aes(x=lat,y=lon), pch=16, size=2,colour="black") + # how to add points part B
-  geom_text(data = input_points,aes(x=lat,y=lon,label=unlist(name.keep))) +
-  ggtitle("Xiaoping Estimates")
+inputdata1 <- inputData[inputData$density>47,] #ask kelly about this number
+colnames(inputdata1)[1:2] <- c('a','b')
+col.plot <- rep('Forest',nrow(inputdata1))
+data_binned1 <-  cut(inputdata1$tot_biomass, breaks, include.lowest = TRUE, labels = FALSE)
+
+inputdata2 <- inputData[inputData$LS>1,] #what should the cutoff be?
+colnames(inputdata2)[1:2] <- c('c','d')
+col.plot2 <- rep('Late Successional',nrow(inputdata1))
+data_binned2 <-  cut(inputdata2$tot_biomass, breaks, include.lowest = TRUE, labels = FALSE)
+
+
+
+inputdata3 <- inputData[inputData$X>430000&inputData$X<557000&inputData$Y>678000&inputData$Y<1200000,]
+colnames(inputdata3)[1:2] <- c('e','f')
+
+d <-   ggplot() +
+  geom_tile(data = inputData, aes(x = X, y = Y,fill = factor(data_binned))) +
+  scale_fill_manual(labels = breaklabels, name = legendName, drop = FALSE, values = colors, guide = "legend") +
+  geom_tile(data = inputdata1, aes(x = a, y = b, colour = 'Forest'), color='black',alpha=0, size=1) +
+    #scale_color_manual(values='black', name=element_blank())
+  geom_tile(data = inputdata1, aes(x = a, y = b, fill = factor(data_binned1))) + #puts biomass colors on
+  geom_tile(data = inputdata2, aes(x = c, y = d, colour = col.plot2), color = 'blue', alpha=0, size=1) +
+    #scale_color_manual(values='blue', name=element_blank())
+  geom_tile(data = inputdata2, aes(x = c, y = d, fill = factor(data_binned2))) + #puts biomass colors on
+  geom_tile(data = inputdata3, aes(x = e, y = f, color = 'Transect'),
+              fill = 'gray', color = NA, size = .25, alpha = .5) +
+  geom_point(data = input_points, aes(x=lat,y=lon), pch=16, size=1, alpha = .9 ,colour="brown") 
+  
+  
+  #+ # how to add points part B
+ # geom_text(data = input_points,aes(x=lat,y=lon,label=unlist(name.keep))) +
+ # ggtitle("Xiaoping Estimates")
 
 add_map_albers <- function(plot_obj, map_data = usFortified, dat){
   p <- plot_obj + geom_path(data = map_data, aes(x = long, y = lat, group = group), size = 0.1) +
@@ -80,5 +125,33 @@ d <- add_map_albers(plot_obj = d, map_data = usFortified, dat = inputData_long)
 quartz()
 print(d)
 
-ggsave(d,filename = 'biomass_est_3_names.pdf')
-dev.off()
+ggsave(d,filename = paste0(Sys.Date(),'biomass_map.pdf'))
+
+
+
+temp <- read.csv('~/Downloads/reclimatedata/tmean_yr_Prism_1900_full.csv')
+precip <- read.csv('~/Downloads/reclimatedata/pr_monthly_Prism_1900_full.csv')
+
+temp.precip <- merge(x = temp, y = precip, by = c('x','y'))
+colnames(inputdata3)[1:2] <- c('x','y')
+temp.precip.bio <- merge(temp.precip,inputdata3,by=c('x','y'))
+
+#temp.precip.bio$Mean*temp.precip.bio$total #smooth metric
+
+LS_binned <- rep('Prairie/Savanna',length(temp.precip.bio$LS))
+LS_binned[temp.precip.bio$density>47] <- 'Forested (No Hemlock)'
+LS_binned[temp.precip.bio$LS>1] <- 'Hemlock'
+
+A <- ggplot()+ labs(x = '',y = "Biomass (Mg/ha)", color = "Ecosystem Type")+geom_jitter(data = temp.precip.bio,aes(x=y,y=tot_biomass,color=LS_binned))
+
+B <- ggplot()+labs(x = '', y = "Mean Temperature [ºC]", color = "Ecosystem Type")+ geom_jitter(data = temp.precip.bio, aes(x=y,y=Mean,color=LS_binned))#+ stat_smooth(data = temp.precip.bio, aes(x = y, y = Mean), method = "lm",
+                                                                                              # formula = y ~ poly(x, 10), se = FALSE) #mean temp
+C <- ggplot()+ labs(x = "Latitude (Albers Projection)", y = "Total Precip [cm]", color = "Ecosystem Type") +geom_jitter(data = temp.precip.bio, aes(x=y,y=total,color=LS_binned))#+ stat_smooth(data = temp.precip.bio, aes(x = y, y = total), method = "lm",
+                                                                              # formula = y ~ poly(x, 10), se = FALSE) #total precip 
+
+library(egg)
+F<-ggarrange(A,B,C, ncol = 1)
+ggsave(F,file='transect_output_scatters.pdf')
+
+
+
