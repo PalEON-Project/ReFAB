8f32e9125c3741c8a906ec666b2ed5ccd6384c65
ANN RAIHO <araiho@nd.edu>
2017-11-16 08:46:51 -0500

    Cleaning up to make final that only makes figure 1 for paper

--- Workflow_Code/Figure_1.R
+++ Workflow_Code/Figure_1.R
@@ -56,13 +56,16 @@ for(i in 1:62){
       #Takes out modern data estimates where data stop# i.e. 'cut' approach
       #samplesList[,1:min(age_index)] <- NA
       
+      if(i!=35){
       not_burn <- 11:200
       biomassCI[[i]] <- apply(samplesList[not_burn,1:100],2,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
       names(biomassCI)[i] <- x.meta[x.meta$site.name == locn,'site.id'][1]
       
       all.samps.list[[i]] <- samplesList[not_burn,1:100]
       
-      all.samps <- rbind(all.samps,samplesList[not_burn,1:100])
+      
+        all.samps <- rbind(all.samps,samplesList[not_burn,1:100])
+      }
       diff.median[[i]] <- apply(diff(t(samplesList[not_burn,1:100])),1,quantile,c(0.5),na.rm=TRUE)
       test <- diff(t(samplesList[not_burn,1:100]))
       lat[[i]] <- x.meta[x.meta$site.name == locn,'lat'][1]
@@ -84,149 +87,105 @@ for(i in 1:62){
   }
 }
 
-lat.lon.df <- data.frame(lat = unlist(lat,use.names = F), lon = unlist(long,use.names = F))
-save(lat.lon.df,file='refab.sites.lat.lon.Rdata')
-     
 
-biomassCI[[2]] <- biomassCI.save[[2]]
 
-save(biomassCI,file='biomassCIRW.Rdata')
 
-biomassCI.real.long <- biomassCI
+biomass.mean.df <- data.frame(lat=unlist(lat)[-35],lon=unlist(long)[-35],biomassMean = unlist(lapply(biomassCI,function(x){mean(x[2,1:11])}))[-c(2,35)])
 
-plot(biomassCI[[1]][2,])
-for(i in 1:62) points(biomassCI[[i]][2,])
+write.csv(biomass.mean.df,file='biomass.means.csv')
 
-biomassCI.save <- biomassCI
 
-blue       <- col2rgb("blue")
-alphablue  <- rgb(blue[1], blue[2], blue[3], 75, max = 255)
-orange       <- col2rgb("orange")
-alphaorange  <- rgb(orange[1], orange[2], orange[3], 85, max = 255)
-
-pdf('compare.RW.v.genPareto.Sigma0.12.pdf')
-par(mfrow=c(2,2))
-plot.new()
-legend('center',c('GenPareto Sigma = .12','RW'),pch=c(19,19),col=c('darkorange','blue'))
+biomass.settle <- last.index <-numeric(62)
 for(i in 1:62){
-  locn <- names(how.many)[i]
-  site_number = unique(x.meta[x.meta$site.name == locn,1])
-  keep.dataset.id <- unique(x.meta[x.meta$site.id==site_number,4])
-  
-  ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
+  if(!is.null(name.keep[[i]])&i!=35){
+    biomass.settle[i] <- x.meta[x.meta$site.name==name.keep[[i]],'SettleBiomassMean'][1]
+    if(!is.na(biomass.settle[i])){
+      last.index[i] <- biomassCI[[i]][2,1]
+    }else{
+      last.index[i] <- NA
+    }
+  }
+  }
 
-  Y = as.matrix(ten_count_use)
-  
-  sample_ages <- x.meta[x.meta[,1] == site_number, ]$age_bacon
-  age_bins <- seq(minAge, maxAge, ageInterval)
-  age_index <- as.matrix(as.numeric(
-    cut(sample_ages, breaks = age_bins, labels=seq(1:(length(age_bins)-1)))
-  ))
-  
-  tmp <- data.frame(cbind(age_index, Y))
-  names(tmp)[1] <- 'age_index'
-  
-  Y2 <- aggregate(tmp, by = list(tmp$age_index), FUN = sum)
-  
-  if(!is.null(group)){
-    Y2 <- Y2[-group.mat[group,],]
-  }
-  
-  Y <- as.matrix(Y2[ , -c(1,2)])
-  age_index <- Y2[,1]
-  
-  plot(biomassCI[[i]][2,],col='blue',typ='l',xlim=c(100,0),
-       ylim=c(0,150),lwd=2,main=names(how.many)[i])
-  ciEnvelope(x = 1:100, yhi = biomassCI[[i]][3,],ylo=biomassCI[[i]][1,],col=alphablue)
-  lines(biomassCI.save[[i]][2,],col='darkorange',lwd=2)
-  ciEnvelope(x = 1:100, yhi = biomassCI.save[[i]][3,],ylo=biomassCI.save[[i]][1,],col=alphaorange)
-  rug(x.meta[x.meta[,1]==site_number,]$age_bacon/100,lwd=2)
-  
-  if(length(out.list[[i]])!=0){
-    points(age_index,seq(5, 145, by = 2)[apply(out.list[[i]],2,which.max)])
-  }
-   
-  mean.keep <- x.meta[x.meta$site.name == locn,'SettleBiomassMean'][1]
-  sd.keep <- x.meta[x.meta$site.name == locn,'SettleBiomassSD'][1]
-  points(0,mean.keep,col='blue',cex=1.5,pch=19)
-  segments(x0=0,y0=mean.keep-sd.keep,x1 = 0,y1=mean.keep+sd.keep)
-  #rug(control.pts[which(control.pts[,2]%in%keep.dataset.id),]$geo_age/100,lwd=3,col="red")
-}
+
+### Creating correlation plot between settlement PLS estimates and settlement ReFAB estiamtes
+last.index <- last.index[-c(which(is.na(last.index)),which(last.index==0))]
+biomass.settle <- biomass.settle[-c(which(is.na(biomass.settle)),which(biomass.settle==0))]
+
+pdf('settle.biomass.corr.pdf')
+par(mfrow=c(1,1))
+plot(biomass.settle,last.index,pch=19,xlab='Settlement Biomass Mean',ylab='Last Prediction Biomass Mean',xlim=c(0,155),ylim=c(0,155))
+abline(a=0,b=1)
+legend('topleft',paste('cor =', cor(x=biomass.settle,y=last.index)),pch=NA)
 dev.off()
 
-#### Finding sites with big decrease near present
-look <- list()
-for(i in 1:length(biomassCI)){
-  if(biomassCI[[i]][2,1] - biomassCI[[i]][2,5] < -5){
-    look[[i]] <- biomassCI[[i]][2,1] - biomassCI[[i]][2,5]#
-    names(look[[i]]) <-name.keep[[i]]
-  }else{
-    look[[i]] <- NA
-  }
-}
-
-sort(unlist(look))
-
-plot(biomassCI[[1]][2,],type='l',ylim=c(0,150))
-for(i in unlist(look)){
-lines(biomassCI[[i]][2,])
-}
-
-
-
-breaks <-  c(seq(0,50,10),seq(75,200,25))
-colors <- rev(terrain.colors(length(breaks)))
-
-breaks.LS <- seq(0,.55,.05)
-colors.LS.palette <- colorRampPalette(c('#99d8c9',
-  '#66c2a4',
-  '#41ae76',
-  '#238b45',
-  '#005824'))
-colors.LS <- c('lightgrey',colors.LS.palette(length(breaks.LS)))
-
-#colors.LS <- c('#ffffcc','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#0c2c84')
-
-#bio.quant <- apply(matrix(unlist(lapply(biomassCI,function(x) x[2,])),61,100,byrow = TRUE),2,quantile,c(0.025,0.5,0.975))
-bio.quant <- apply(all.samps,2,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
-
-diff.mat.all<- t(diff(t(all.samps[,100:1])))
-
-stop.spot <- list()
-for(i in 1:62){
-  stop.spot[[i]] <- min(age.keep[[i]],na.rm = TRUE)
-}
-
+### Mapping sites by the last data point to make sure there is no spatial correlation in end of data
 map('state', xlim=c(-98,-81), ylim=c(41.5,50))
 points(unlist(long),unlist(lat), pch=19,
        cex=1.1,lwd=.2,col=rainbow(20,start = 0,end = .8)[unlist(stop.spot)])
 legend('topright',as.character(1:20),col=rainbow(20,start = 0,end = .8),pch=rep(19,20))
 title('Map Colored by Last Sample Age Index')
 
-how.much <- matrix(NA,length(not_burn),99)
-median.bucket <- matrix(NA,length(not_burn),100)
-for(i in 1:99){
-  for(r in 1:length(not_burn)){
-    div.num <- 61 - length(which(is.na(diff.mat.all[seq(1+r,61*length(not_burn),length(not_burn)),i])))
-    if(div.num==0) div.num <- 61
-    how.much[r,i] <- length(which(diff.mat.all[seq(1+r,61*length(not_burn),length(not_burn)),i]>0))/div.num
-  }
+#####
+##### Calculate derived quantities for plotting #####
+#####
+
+bio.quant <- apply(all.samps,2,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
+
+diff.mat.all<- t(diff(t(all.samps[,100:1])))
+
+
+## Find cutoff for time series by last data point
+stop.spot <- list()
+for(i in 1:62){
+  stop.spot[[i]] <- min(age.keep[[i]],na.rm = TRUE)
 }
 
+## Looking for where to cut all.samps matrix -- changes with number of sites
+site_count <- 61-1
+
+## calculation for proportion sites increasing -- barplot
+how.much <- matrix(NA,length(not_burn),99)
+for(i in 1:99){
+  for(r in 1:length(not_burn)){
+    div.num <- site_count - length(which(is.na(diff.mat.all[seq(1+r,site_count*length(not_burn),length(not_burn)),i])))
+    if(div.num==0) div.num <- site_count
+    how.much[r,i] <- length(which(diff.mat.all[seq(1+r,site_count*length(not_burn),length(not_burn)),i]>0))/div.num
+  }
+}
 #calculate the probability of increase for each site then make a boxplot of all of the sites probability of increase at each time.
 #proportion of medians that are greater than zero
 save.how.much <- apply(how.much,2,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
 
-for(i in 1:100){
-  for(r in 1:length(not_burn)){
-     median.bucket[r,i] <- quantile(all.samps[seq(1+r,61*length(not_burn),length(not_burn)),i],c(.5),na.rm = TRUE)
-  }
-}
-save.median.bucket <- apply(median.bucket,2,quantile,c(0.025,0.5,0.975),na.rm =TRUE)
 
+## calculation for median line over time series -- purple line
+median.bucket <- matrix(NA,length(not_burn),100)
+
+for(r in 1:length(not_burn)){
+    samps.vec.list <- lapply(all.samps.list,function(x){return(x[r,])})
+    means.iter <- do.call(rbind,samps.vec.list)
+    median.bucket[r,] <- apply(means.iter, 2, median)#colMeans(means.iter,na.rm = TRUE)#apply(means.iter,2,quantile,probs=c(.5),na.rm =TRUE)
+}
+
+save.median.bucket <- apply(median.bucket,2,quantile,c(0.025,0.5,0.975),na.rm =TRUE)
+save.median.bucket[2,] <- colMeans(median.bucket)
+
+## biomass breaks
+breaks <-  c(seq(0,50,10),seq(75,200,25))
+colors <- rev(terrain.colors(length(breaks)))
+
+## late successional breaks
+breaks.LS <- seq(0,.55,.05)
+colors.LS.palette <- colorRampPalette(c('#99d8c9','#66c2a4','#41ae76','#238b45','#005824')) #colors found using Rcolorbrewer
+colors.LS <- c('lightgrey',colors.LS.palette(length(breaks.LS)))
+
+## binning for colors
 data_binned <-  cut(save.median.bucket[2,], c(breaks), include.lowest = FALSE, labels = FALSE)
 data_binned_LS <-  cut(unlist(hem.is), c(breaks.LS), include.lowest = TRUE, labels = FALSE)
   
+##### 
+##### Creating Figure 1 ##### 
+##### 
 
 pdf(paste0('average.biomass_.12_',Sys.Date(),'.pdf'))
 #quartz()
@@ -277,49 +236,61 @@ points(seq(.5,98.5,1),save.how.much[2,],pch=19,cex=.5)
 abline(h=.5)
 dev.off()
 
+### Old code for showing RW and genpareto outputs together
+blue       <- col2rgb("blue")
+alphablue  <- rgb(blue[1], blue[2], blue[3], 75, max = 255)
+orange       <- col2rgb("orange")
+alphaorange  <- rgb(orange[1], orange[2], orange[3], 85, max = 255)
 
-
-
-site_number = unique(x.meta[x.meta$site.name == "Wood Lake",1])
-ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
-#capitola 38
-#kelly's 34
-#wood 33
-
-lat.long.reg <- cbind(unlist(long),unlist(lat))
-lat.long.reg.df = data.frame(lat.long.reg)
-colnames(lat.long.reg.df) = c('x', 'y')
-
-coordinates(lat.long.reg.df) <- ~ x + y
-proj4string(lat.long.reg.df) <- CRS('+proj=longlat +ellps=WGS84')
-
-albers <- spTransform(lat.long.reg.df, CRS('+init=epsg:3175'))
-albers <- as.matrix(data.frame(albers))
-
-
-map('state', xlim=c(-98,-81), ylim=c(41.5,50))
-points(unlist(long),unlist(lat), pch=19,
-       cex=1.1,lwd=.2)
-text(unlist(long),unlist(lat),unlist(name.keep),cex=.5)
-
-pdf('boxplots.prob.of.inc.pdf')
-boxplot(prob.of.inc,xlim=c(100,1))
-abline(h=.5)
-title('Probability of Increase')
-dev.off()
-
-pdf('barplot.of.prop.inc.pdf')
-diff.median.mat <- matrix(unlist(diff.median),99,62)
-bars <- numeric(99)
-for(t in 1:99){
-  bars[t] <- length(which(diff.median.mat[t,]<0))/62
+pdf('compare.RW.v.genPareto.Sigma0.12.pdf')
+par(mfrow=c(2,2))
+plot.new()
+legend('center',c('GenPareto Sigma = .12','RW'),pch=c(19,19),col=c('darkorange','blue'))
+for(i in 1:62){
+  locn <- names(how.many)[i]
+  site_number = unique(x.meta[x.meta$site.name == locn,1])
+  keep.dataset.id <- unique(x.meta[x.meta$site.id==site_number,4])
+  
+  ten_count_use = ten.count[which(x.meta$site.id == site_number), ]
+  
+  Y = as.matrix(ten_count_use)
+  
+  sample_ages <- x.meta[x.meta[,1] == site_number, ]$age_bacon
+  age_bins <- seq(minAge, maxAge, ageInterval)
+  age_index <- as.matrix(as.numeric(
+    cut(sample_ages, breaks = age_bins, labels=seq(1:(length(age_bins)-1)))
+  ))
+  
+  tmp <- data.frame(cbind(age_index, Y))
+  names(tmp)[1] <- 'age_index'
+  
+  Y2 <- aggregate(tmp, by = list(tmp$age_index), FUN = sum)
+  
+  if(!is.null(group)){
+    Y2 <- Y2[-group.mat[group,],]
+  }
+  
+  Y <- as.matrix(Y2[ , -c(1,2)])
+  age_index <- Y2[,1]
+  
+  plot(biomassCI[[i]][2,],col='blue',typ='l',xlim=c(100,0),
+       ylim=c(0,150),lwd=2,main=names(how.many)[i])
+  ciEnvelope(x = 1:100, yhi = biomassCI[[i]][3,],ylo=biomassCI[[i]][1,],col=alphablue)
+  lines(biomassCI.save[[i]][2,],col='darkorange',lwd=2)
+  ciEnvelope(x = 1:100, yhi = biomassCI.save[[i]][3,],ylo=biomassCI.save[[i]][1,],col=alphaorange)
+  rug(x.meta[x.meta[,1]==site_number,]$age_bacon/100,lwd=2)
+  
+  if(length(out.list[[i]])!=0){
+    points(age_index,seq(5, 145, by = 2)[apply(out.list[[i]],2,which.max)])
+  }
+  
+  mean.keep <- x.meta[x.meta$site.name == locn,'SettleBiomassMean'][1]
+  sd.keep <- x.meta[x.meta$site.name == locn,'SettleBiomassSD'][1]
+  points(0,mean.keep,col='blue',cex=1.5,pch=19)
+  segments(x0=0,y0=mean.keep-sd.keep,x1 = 0,y1=mean.keep+sd.keep)
+  #rug(control.pts[which(control.pts[,2]%in%keep.dataset.id),]$geo_age/100,lwd=3,col="red")
 }
-
-barplot(rev(bars))
-abline(h=.5)
-title('Proportion Median Increasing')
 dev.off()
-
 
 
 
