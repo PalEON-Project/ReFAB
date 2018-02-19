8089370db0ee29a9eabbd79c709b872841f9cf9a
ANN RAIHO <araiho@nd.edu>
2018-02-19 13:03:09 -0500

    increasing speed of calibration model

--- Workflow_Code/calibration.model.R
+++ Workflow_Code/calibration.model.R
@@ -18,62 +18,17 @@ calib_code <- nimbleCode({
         beta2[r,i] ~ dnorm(0,.04)
       }  
     }
-    
-    phi1[,] <- Z[,]%*%beta1[,]
-    phi2[,] <- Z[,]%*%beta2[,]
-    
-    for(j in 1:J){
-      for(i in 1:I){
-        shape1[j,i] <- exp(phi1[j,i])
-        shape2[j,i] <- exp(phi2[j,i])
-      }
-    }
-    
+
+    shape1 <- exp(Z[,] %*% beta1[,])
+    shape2 <- exp(Z[,] %*% beta2[,])
+
     for(j in 1:J){
       Y[j, 1] ~ dbetabin(shape1[j, 1], shape2[j, 1], n[j])
       for(i in 2:(I-1)){
         Y[j, i] ~ dbetabin(shape1[j, i], shape2[j, i], n[j] - sum(Y[j,1:(i-1)]))
       }
     }
-    
-    # for(j in 1:J){
-    #   p.true[j,1] ~ dbeta(exp1[j,1],exp2[j,1])
-    #   p.rel[j,1] <- p.true[j,1]
-    #   
-    #   for(i in 2:(I-1)){
-    #     p.rel[j,i]  ~ dbeta(exp1[j,i],exp2[j,i]) 
-    #     p.true[j,i] <-  p.rel[j,i] * (1 - sum(p.true[j,1:(i-1)]))
-    #   }	
-    #   p.true[j,I] <- 1 - sum(p.true[j,1:(I-1)])
-    # }  
-    
-    
-    # for(j in 1:J){
-    #   Y[j,] ~ dmulti(size = n[j], prob = p.true[j,])
-    # }
-    
-    # Removing Derived Quantity to increase MCMC speed
-    # phi.first1[,] <- Z.new[,]%*%beta[,]
-    # pine.phi1[,] <- Z.new[,]%*%beta.pine[,]
-    # 
-    # for(j in 1:145){
-    #   for(i in 1:I){
-    #     exp.phi1[j,i] <- exp(phi.first1[j,i])
-    #     exp.pine.phi1[j,i] <- exp(pine.phi1[j,i])
-    #   }
-    # }
-    # 
-    # for(j in 1:145){
-    #   p.true1[j,1] ~ dbeta(exp.phi1[j,1],exp.pine.phi1[j,1])
-    #   p.rel1[j,1] <- p.true1[j,1]
-    #   
-    #   for(i in 2:(I-1)){
-    #     p.rel1[j,i]  ~ dbeta(exp.phi1[j,i],exp.pine.phi1[j,i]) 
-    #     p.true1[j,i] <-  p.rel1[j,i] * (1 - sum(p.true1[j,1:(i-1)]))
-    #   }	
-    #   p.true1[j,I] <- 1 - sum(p.true1[j,1:(I-1)])
-    # }  
-    
+
   })
 
 #load("Data/calibration.data.Rdata")
@@ -81,7 +36,6 @@ model.dir <- c('Workflow Code/')
 fig.dir <- c("Figures/")
 source("Workflow_Code/utils/bs_nimble.R")
 
-p = matrix(NA,nrow(Y),ncol(Y)) ; phi = p
 J = nrow(Y)
 
 data = list(Y = as.matrix(Y), Z =  Z.knots)
@@ -92,9 +46,10 @@ constants = list(n = rowSums(Y), R = ncol(Z.knots), I = ncol(Y),
 inits = list(beta1 = matrix(1, ncol(Z.knots), ncol(Y)),
              beta2 = matrix(1, ncol(Z.knots), ncol(Y)))
 
-dimensions = list(shape1 = dim(phi), shape2 = dim(phi),
-                  phi1 = dim(phi), phi2 = dim(phi), 
-                  Z = dim(Z.knots), beta1 = c(ncol(Z.knots), ncol(Y)), 
+dimensions = list(shape1 = c(nrow(Y),ncol(Y)),
+                  shape2 = c(nrow(Y),ncol(Y)), 
+                  Z = dim(Z.knots), 
+                  beta1 = c(ncol(Z.knots), ncol(Y)), 
                   beta2 = c(ncol(Z.knots), ncol(Y)),
                   Y = dim(Y), n = nrow(Y))
 
