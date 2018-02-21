cde354047d30e108984e7a241988a4b7b0fee421
ANN RAIHO <araiho@nd.edu>
2018-02-21 09:54:31 -0600

    Fixing knots and adding a scenario for no group_rm set as group_rm = 11

--- run_calib_valid.R
+++ run_calib_valid.R
@@ -6,6 +6,10 @@ if (is.na(arg[1])) {
   group_rm <- as.numeric(arg[1])
 }
 
+if(group_rm == 11){
+  group_rm <- NA
+}
+
 ### after get.data
 
 library(nimble)
@@ -27,10 +31,10 @@ trees <- c("FAGUS","TSUGAX","QUERCUS","BETULA",
            'PINUSX',"JUGLANSX","ACERX","FRAXINUX",
            "OSTRYCAR","ULMUS","TILIA","ALNUSX",
            "CYPERACE","PICEAX",
-           "ABIES","POPULUS","CARYA","LARIXPSEU","CUPRESSA","TAXUS","NYSSA",
-           "CASTANEA","PLATANUS","SALIX",
-           "LIQUIDAM")
-other.trees <- c()#NULL#c()
+           "ABIES","POPULUS","CARYA","LARIXPSEU","CUPRESSA")
+other.trees <- c("TAXUS","NYSSA",
+                 "CASTANEA","PLATANUS","SALIX",
+                 "LIQUIDAM")#NULL#c()
 drop.taxa <- NA#c('other_herbs')
 
 source('taxa_selection.R')
@@ -38,10 +42,10 @@ Y <- taxa_selection(trees = trees, other.trees = other.trees,
                     cast.x = cast.x, sites_rm = sites_rm,
                     all.pollen.taxa.names = all.pollen.taxa.names,
                     prairie.include = T, other.herbs.include = T,
-                    other.trees.include = F, drop.taxa = drop.taxa,
+                    other.trees.include = T, drop.taxa = drop.taxa,
                     PFT.do = F)
 
-Niters <- 2000
+Niters <- 5000
 bMax <- 143
 
 #### Setting up 10 fold cross validation
@@ -53,8 +57,8 @@ Y.calib <- Y[-sets10[,group_rm],]; Y.pred <- Y[sets10[,group_rm],]
 biomass.calib <- biomass[-sets10[,group_rm]]; biomass.pred <- biomass[sets10[,group_rm]]
 
 #### Making sure Z.knots and u are the same between calibration and validation
-Z.knots = bs(biomass.calib,intercept=TRUE,df=5)
-u <- c(rep(attr(Z.knots,"Boundary.knots")[1],1),attr(Z.knots,"knots"),rep(attr(Z.knots,"Boundary.knots")[2],1))
+Z.knots = Z.knots = bs(biomass.calib, intercept=TRUE, knots = 30, Boundary.knots=c(0,150))
+u <- c(0,30,150) #c(rep(attr(Z.knots,"Boundary.knots")[1],1),attr(Z.knots,"knots"),rep(attr(Z.knots,"Boundary.knots")[2],1))
 
 source(file.path('Workflow_Code','calibration.model.R'))
 samples.mixed <- calibration_model(Y = Y.calib, biomass = biomass.calib,
