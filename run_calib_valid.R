4914315c7513ba2a2e3c8acecb7b9fab44bc5c28
ANN RAIHO <araiho@nd.edu>
2018-02-28 13:47:27 -0500

    adding script to run prediction on gold standard 1/3 that has been left out during calibration model selection

--- run_calib_valid.R
+++ run_calib_valid.R
@@ -46,7 +46,7 @@ Y <- taxa_selection(trees = trees, other.trees = other.trees,
                     PFT.do = F)
 
 Niters <- 5000
-bMax <- 143
+bMax <- 150
 
 #### Setting up 10 fold cross validation
 set.seed(5)
@@ -57,7 +57,7 @@ Y.calib <- Y[-sets10[,group_rm],]; Y.pred <- Y[sets10[,group_rm],]
 biomass.calib <- biomass[-sets10[,group_rm]]; biomass.pred <- biomass[sets10[,group_rm]]
 
 #### Making sure Z.knots and u are the same between calibration and validation
-Z.knots = Z.knots = bs(biomass.calib, intercept=TRUE, knots = 30, Boundary.knots=c(0,150))
+Z.knots = bs(biomass.calib, intercept=TRUE, knots = 30, Boundary.knots=c(0,150))
 u <- c(0,30,150) #c(rep(attr(Z.knots,"Boundary.knots")[1],1),attr(Z.knots,"knots"),rep(attr(Z.knots,"Boundary.knots")[2],1))
 
 source(file.path('Workflow_Code','calibration.model.R'))
@@ -84,11 +84,11 @@ calibration.figs(bMax = bMax, Z.knots = Z.knots, Y = Y.keep,
 if(group_rm == 10){
   samples.pred.mat <- matrix(NA,nrow(samples.pred),length(biomass.keep))
   for(i in 1:10){
-    load(file = file.path('~/Downloads','samps.27',paste0('samples.pred.group',i,'.Rdata')))
+    load(file = file.path('~/Downloads','samps.22',paste0('samples.pred.group',i,'.Rdata')))
     #load(file = file.path(paste0('samples.pred.group',i,'.Rdata')))
     samples.pred.mat[,sets10[,i]] <- samples.pred[,grep('b',colnames(samples.pred))]
   }
-  pdf(paste0('validation.r2.7knots.pdf'))
+  pdf(paste0('10.fold.r2.validation.pdf'))
   par(mfrow=c(1,1))
   plot(biomass, colMeans(samples.pred.mat, na.rm = T),
        xlim=c(0,bMax), ylim=c(0,bMax), pch=19,
