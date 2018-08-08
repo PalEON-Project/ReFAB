bf754362ed48ee7a9d3676fea2afdc99262ca97b
ANN RAIHO <araiho@nd.edu>
2018-08-08 16:32:07 -0400

    deleted parts that don't belong in file

--- Workflow_Code/[6.3.2]bimodal_comp.R
+++ Workflow_Code/[6.3.2]bimodal_comp.R
@@ -38,67 +38,8 @@ for(i in 1:length(bimodal_sites)){
   }
 dev.off()
 
-out.keep <- list()
-for(i in 1:20){
-  load(paste0('~/Downloads/workInfo-2/workInfo_',1160+i,
-       '_Wintergreen-Lake_Beta_',i,'.Rdata'))
-  out.keep[[i]] <- out
-}
-load('prediction.data.Rdata')
 
-locn <- 'Wintergreen Lake'
+source(file.path('Workflow_Code','utils','compare_workInfo.R'))
+compare_workInfo(path_to_workInfo = '~/Downloads/work_age/',
+                 locn = 'Wintergreen Lake')
 
-minAge = 0
-maxAge = 10000
-ageInterval = 100
-
-site_number = unique(x.meta[x.meta$site.name == locn,1])
-x.meta.use <- x.meta[x.meta$site.name == locn,]
-
-source('test_site.R')
-test_site(x.meta.use)
-
-ten_count_use = ten.count[which(x.meta$site.name == locn), ]
-ten_count_use[which(is.na(ten_count_use))] <- 0
-Y = as.matrix(ten_count_use)
-
-sample_ages <- x.meta.use$age_bacon
-age_bins <- seq(minAge, maxAge, ageInterval)
-age_index <- as.matrix(as.numeric(
-  cut(sample_ages, breaks = age_bins, labels=seq(1:(length(age_bins)-1)))
-))
-
-tmp <- data.frame(cbind(age_index, Y))
-names(tmp)[1] <- 'age_index'
-
-Y2 <- aggregate(tmp, by = list(tmp$age_index), FUN = sum)
-dim(Y2)
-
-pdf('wintergreen_prop_liks.pdf')
-bMax <- 150
-par(mfrow=c(2,3))
-for(i in 1:(ncol(out)-1)){
-  plot(as.numeric(Y2[i,3:24]/sum(Y2[i,3:24])),typ='o', ylab = 'Pollen Prop', xlab = NA, xaxt = 'n')
-  points(as.numeric(Y2[i+1,3:24]/sum(Y2[i+1,3:24])),typ='o', ylab = 'Pollen Prop',
-         xlab = NA,xaxt = 'n',col='red')
-  axis(side = 1,at = 1:22,labels = colnames(Y),las=2,cex=1)
-  
-  plot(seq(5, bMax-5, by = 2),
-       exp(out.keep[[b]][,i]-max(out.keep[[b]][,i]))/-sum(out.keep[[b]][,i]),
-       typ='l',main=paste('age',Y2$age_index[i]),ylab='Likelihood')
-  for(b in 1:20){
-    points(seq(5, bMax-5, by = 2),
-           exp(out.keep[[b]][,i]-max(out.keep[[b]][,i]))/-sum(out.keep[[b]][,i]),
-           typ='l')
-  }
-  
-  plot(seq(5, bMax-5, by = 2),
-       exp(out.keep[[b]][,i+1]-max(out.keep[[b]][,i+1]))/-sum(out.keep[[b]][,i+1]),
-       typ='l',main=paste('age',Y2$age_index[i+1]),col='red',ylab='Likelihood')
-  for(b in 1:20){
-   points(seq(5, bMax-5, by = 2),
-          exp(out.keep[[b]][,i+1]-max(out.keep[[b]][,i+1]))/-sum(out.keep[[b]][,i+1]),
-          typ='l',col='red')
-  }
-}
-dev.off()
