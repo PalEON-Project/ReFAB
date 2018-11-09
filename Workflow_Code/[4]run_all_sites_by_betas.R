9ea956e6540c86095a40017ad9f018b5748e4cf0
ANN RAIHO <araiho@nd.edu>
2018-11-08 20:22:33 -0500

    getting ready to run version 4

--- Workflow_Code/[4]run_all_sites_by_betas.R
+++ Workflow_Code/[4]run_all_sites_by_betas.R
@@ -12,9 +12,11 @@ dataDir <- c(getwd()) #or wherever allPredData.Rda is located
 ## we'll have length(runnum) == nrow(beta.est)*length(sites)
 
 #dataID <- read.csv(file.path('Cross_Validation','dataID.csv')) #for 10K
-dataID <- read.csv(file.path('Cross_Validation','beta_run_dataID.csv')) #for paleon mip
+#dataID <- read.csv(file.path('Cross_Validation','beta_run_dataID.csv')) #for paleon mip
+dataID <- read.csv('dataID_bacon_v4_means.csv') #for paleon mip
 
-load('prediction.data_v2.Rdata')
+
+load('prediction.data_v4.Rdata')
 ####
 #### Master Setup
 ####
@@ -37,18 +39,35 @@ control.pts<-read.csv(file.path('Data','control.pts.csv'))
 # source(file.path('Workflow_Code','prep_data.R'))
 # load(file.path(dataDir,'allPredData.Rda')) # load for 10K run
 # load('paleon.data.Rdata') # adding via ssh rather than github
+bMax <- 227
+u <- c(1,47,227)
+new.biomass <- 1:bMax
+Z = matrix(0,nrow=length(new.biomass),ncol=5)
+for(i in 1:length(new.biomass)){
+  u_given <- new.biomass[i]
+  Z[i,] = bs_nimble(u_given, u=u, N0 = rep(0, (length(u)-1)),
+                        N1 = rep(0, (length(u))), 
+                        N2 = rep(0, (length(u)+1)), 
+                        N3 = rep(0, (length(u)+2)))
+}
 
-load(file = paste0("beta.est.group.inTWOTHIRDS_150.Rdata")) #load via ssh
+load(file = paste0("beta.est.group.inNA.Rdata")) #load via ssh
 
 i.beta1 <- grep("beta1",colnames(samples.mixed))
 i.beta2 <- grep("beta2",colnames(samples.mixed))
+burnin <- .2*nrow(samples.mixed)
 
 beta <- dataID[dataID$ID==runnum,'beta']
 
-Nbeta <- round(seq(8000,nrow(samples.mixed),length.out = 20))[beta]
-
-beta1.est.real = matrix(samples.mixed[Nbeta,i.beta1],5,ncol(ten.count))
-beta2.est.real = matrix(samples.mixed[Nbeta,i.beta2],5,ncol(ten.count))
+if(!is.na(beta)){
+  Nbeta <- round(seq(8000,nrow(samples.mixed),length.out = 20))[beta]
+  
+  beta1.est.real = matrix(samples.mixed[Nbeta,i.beta1],ncol(Z),ncol(ten.count))
+  beta2.est.real = matrix(samples.mixed[Nbeta,i.beta2],ncol(Z),ncol(ten.count))
+}else{
+  beta1.est.real = matrix(colMeans(samples.mixed[burnin:nrow(samples.mixed),i.beta1]),ncol(Z),ncol(Y))
+  beta2.est.real = matrix(colMeans(samples.mixed[burnin:nrow(samples.mixed),i.beta2]),ncol(Z),ncol(Y))
+}
 
 #beta1.est.real = matrix(colMeans(samples.mixed[,i.beta1]),5,ncol(ten.count))
 #beta2.est.real = matrix(colMeans(samples.mixed[,i.beta.pine]),5,ncol(ten.count))
@@ -75,7 +94,12 @@ site_number = unique(x.meta[x.meta$site.name == locn,1])
 x.meta.use <- x.meta[x.meta$site.name == locn,]
 
 x.bacon.use <- x.bacon[x.meta$site.name == locn,]
-which_bacon <- seq(2,500,20)[dataID[dataID$ID==runnum,'beta']]
+if(!is.na(beta)){
+  which_bacon <- seq(2,500,20)[dataID[dataID$ID==runnum,'beta']]
+  sample_ages <- x.bacon.use[,which_bacon]#x.meta.use$age_bacon
+}else{
+  sample_ages <- rowMeans(x.bacon.use)
+}
 
 source(file.path('Workflow_Code','utils','test_site.R'))
 test_site(x.meta.use)
@@ -84,7 +108,6 @@ ten_count_use = ten.count[which(x.meta$site.name == locn), ]
 ten_count_use[which(is.na(ten_count_use))] <- 0
 Y = as.matrix(ten_count_use)
 
-sample_ages <- x.bacon.use[,which_bacon]#x.meta.use$age_bacon
 age_bins <- seq(minAge, maxAge, ageInterval)
 age_index <- as.matrix(as.numeric(
   cut(sample_ages, breaks = age_bins, labels=seq(1:(length(age_bins)-1)))
@@ -114,11 +137,14 @@ smp <- fit_fix_sigma(locn = locn, pred_code_fix_sigma = pred_code_fix_sigma,
 		                 control.pts = control.pts, sigma = sigma,
                      group = group, group.mat = group.mat, lik.only = FALSE,
                      maxAge = 10000, Nbeta = beta, ID = runnum,
-		                 liks.by.taxa = TRUE, bMax = 150)
+		                 liks.by.taxa = TRUE, bMax = bMax)
 
 stop()
 
-source('site_diag.R')
+source('Workflow_Code/utils/site_diag.R')
+site_diag(locn = locn,x.meta=x.meta,ten.count = ten.count,
+          path_to_samps = '~/ReFAB/',path_to_Info = '~/ReFAB/')
+
 
 ## Full Site Diagnostics
 ## demont, chippewa bog, emrick, ##frains, radtke, wintergreen
@@ -142,7 +168,7 @@ site_diag(locn = 'Lake Mendota', x.meta = x.meta, ten.count = ten.count,
 
 ## Simple Site Diagnositics
 plot(colMeans(samplesList[,1:100]),col='red',pch=19,ylim=c(0,150))
-points(age_index,seq(5, 150-5, by = 2)[apply(out,2,which.max)])
+points(age_index,seq(5, bMax-5, by = 2)[apply(out,2,which.max)])
 
 ## Drawing Multiple MCMCs
 load("~/Downloads/samps/samplesList_workInfo_181_Cub-Lake_Beta_1.Rda")
@@ -160,13 +186,13 @@ pdf('Cub-Lake-MCMC.pdf')
 par(mfrow=c(2,2))
 for(i in 1:100){
   plot(samplesList[,i],typ='l',lwd=1.5,ylim=c(0,bMax))
-  points(samplesList.save[,i],typ='l',lwd=1.5,col='red') 
-  points(samplesList.save.1[,i],typ='l',lwd=1.5,col='blue') 
+  #points(samplesList.save[,i],typ='l',lwd=1.5,col='red') 
+  #points(samplesList.save.1[,i],typ='l',lwd=1.5,col='blue') 
   title(i)
   if(any(i==age_index)){
-    abline(h = seq(5, 150-5, by = 2)[apply(out,2,which.max)][which(i==age_index)],lwd=2)
-    abline(h = seq(5, 150-5, by = 2)[apply(out.save,2,which.max)][which(i==age_index)],col='red',lwd=2)
-    abline(h = seq(5, 150-5, by = 2)[apply(out.save.1,2,which.max)][which(i==age_index)],col='blue',lwd=2)
+    abline(h = seq(5, bMax-5, by = 2)[apply(out,2,which.max)][which(i==age_index)],lwd=2)
+    #abline(h = seq(5, bMax-5, by = 2)[apply(out.save,2,which.max)][which(i==age_index)],col='red',lwd=2)
+    #abline(h = seq(5, bMax-5, by = 2)[apply(out.save.1,2,which.max)][which(i==age_index)],col='blue',lwd=2)
   }
 }
 dev.off()
