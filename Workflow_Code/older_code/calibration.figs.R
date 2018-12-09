9da845c1af476170e73c194e97b7d416036adcec
ANN RAIHO <araiho@nd.edu>
2018-12-09 13:54:48 -0500

    Hacking to work with other data

--- Workflow_Code/older_code/calibration.figs.R
+++ Workflow_Code/older_code/calibration.figs.R
@@ -132,11 +132,14 @@ calibration.figs <- function(bMax, Z.knots, Y, samples.mixed, outLik,
   #beta1 = matrix(samples.mixed[iter.look,i.beta1],ncol(Z),ncol(Y))
   #beta2 = matrix(samples.mixed[iter.look,i.beta2],ncol(Z),ncol(Y))
   
-  beta1 = matrix(colMeans(samples.mixed[,i.beta1]),ncol(Z),ncol(Y))
-  beta2 = matrix(colMeans(samples.mixed[,i.beta2]),ncol(Z),ncol(Y))
+  i.beta1 <- grep(pattern = 'beta1',colnames(samples.mixed))
+  i.beta2 <- grep(pattern = 'beta2',colnames(samples.mixed))
   
-  alpha <- exp(Z.new%*%beta1)
-  beta <- exp(Z.new%*%beta2)
+  beta1 = matrix(colMeans(samples.mixed[,i.beta1]),ncol(Z.new),ncol(Y))
+  beta2 = matrix(colMeans(samples.mixed[,i.beta2]),ncol(Z.new),ncol(Y))
+  
+  alpha <- linexp(Z.new%*%beta1,J=bMax,I=ncol(Y))
+  beta <- linexp(Z.new%*%beta2,J=bMax,I=ncol(Y))
   
   mean.betas.ex <- alpha/(alpha+beta)
   sd.betas.ex <- sqrt(alpha*beta/((alpha+beta)^2*(alpha+beta+1)))
@@ -151,28 +154,43 @@ calibration.figs <- function(bMax, Z.knots, Y, samples.mixed, outLik,
   mean.betas.ex[,ncol(Y)] <- 1 - rowSums(mean.betas.ex[,1:(ncol(Y)-1)])
   
   props <- prop.table(as.matrix(Y),1)
-  props.pred <- prop.table(as.matrix(Y.pred),1)
+  props.pred <- prop.table(as.matrix(Y),1)
   
   #outlier <- as.numeric(names(sort(abs(biomass-colMeans(samples.pred[,grep('b',colnames(samples.pred))])))))[90]
   
-  pdf(paste0('outlier.splines.group.',group_rm,'.pdf'))
-  pdf('splines.together.pdf')
+  #pdf(paste0('outlier.splines.group.',group_rm,'.pdf'))
+  #pdf('splines.together.pdf')
+  pdf('splines.new_data.pdf')
   par(mfrow=c(2,2))
   for(i in 1:ncol(Y)){
-    plot(1:bMax, mean.betas[,i],
-         ylim=c(0,max(mean.betas[,i] + sd.betas[,i]*2,
-                      mean.betas[,i] - sd.betas[,i]*2, 
+    if(i %in% seq(1,22,3)){
+      plot.new()
+      legend('center',c('PLS Biomass','ReFAB Biomass','PLS Biomass, non outlier'),pch=c(19,19,1),
+             col=c('blue','red','black'),title='R2 Outliers')
+    }
+    plot(1:bMax, mean.betas.ex[,i],
+         ylim=c(0,max(mean.betas.ex[,i] + sd.betas.ex[,i]*2,
+                      mean.betas.ex[,i] - sd.betas.ex[,i]*2, 
                       props[,i])),
-         main = colnames(Y)[i],pch=19)
-    points(1:bMax, mean.betas.ex[,i],col='red')
-    ciEnvelope(x = 1:bMax, yhi = mean.betas[,i] + sd.betas[,i]*2,
-               ylo = mean.betas[,i] - sd.betas[,i]*2,col = alphablue)
-    
+         main = colnames(Y)[i],pch=19,
+         ylab = 'Pollen Proporation',
+         xlab = 'Biomass (Mg/ha)')
+    abline(v=u)
+   # points(1:bMax, mean.betas.ex[,i],col='red')
     ciEnvelope(x = 1:bMax, yhi = mean.betas.ex[,i] + sd.betas.ex[,i]*2,
-               ylo = mean.betas.ex[,i] - sd.betas.ex[,i]*2,col = alphaorange)
+               ylo = mean.betas.ex[,i] - sd.betas.ex[,i]*2,col = adjustcolor('blue',alpha=.5))
+    
+    #ciEnvelope(x = 1:bMax, yhi = mean.betas.ex[,i] + sd.betas.ex[,i]*2,
+    #           ylo = mean.betas.ex[,i] - sd.betas.ex[,i]*2,col = alphaorange)
     points(biomass,props[,i])
+    #points(biomass[bimodal_sites],props[,i][bimodal_sites],pch=19,col='blue')
+    #points(colMeans(samples.pred)[bimodal_sites],props[,i][bimodal_sites],pch=19,col='red')
+    
+    textxy(biomass,props[,i],1:length(biomass),offset = 0)
+    #textxy(colMeans(samples.pred)[bimodal_sites],props[,i][bimodal_sites],bimodal_sites,offset = 0)
+    
     #points(biomass.pred,props.pred[,i],col='red',pch=19)
-    legend('topright',c('bMax = 150','bMax = 143'),pch=c(19,19),col=c(alphablue,alphaorange))
+    #legend('topright',c('bMax = 150','bMax = 143'),pch=c(19,19),col=c(alphablue,alphaorange))
     ###Validation outlier
     if(FALSE){
     points(biomass.pred[outlier],
@@ -209,7 +227,8 @@ calibration.figs <- function(bMax, Z.knots, Y, samples.mixed, outLik,
 if(FALSE){  
 prop.quants <- matrix(NA,ncol(samples.mixed),3)
 for(i in 1:ncol(samples.mixed)){
-  prop.quants[i,]<-quantile(samples.mixed[,i],c(.025,.5,.975),na.rm=TRUE)
+  prop.quants[i,]<-quantile(samples.mixed[,i],c(.025,.5,.975),
+                            na.rm=TRUE)
 }
 rownames(prop.quants)<-colnames(samples.mixed)
 
@@ -221,7 +240,10 @@ par(mfrow=c(2,2))
 for(i in 1:22){
   plot(1:145,prop.quants[grep('p.true1',
                               colnames(samples.mixed))[(plot.help[i]+1):plot.help[i+1]],2],
-       pch=21,bg='gray',main=colnames(counts)[i],ylim=range(prop.quants[grep('p.true1',colnames(samples.mixed))[(plot.help[i]+1):plot.help[i+1]],]),ylab='pollen prop',xlab='biomass')
+       pch=21,bg='gray',main=colnames(counts)[i],
+       ylim=range(prop.quants[grep('p.true1',
+                                   colnames(samples.mixed))[(plot.help[i]+1):plot.help[i+1]],]),
+       ylab='pollen prop',xlab='biomass')
   
   ciEnvelope(x = 1:145,
              ylo = prop.quants[grep('p.true1',colnames(samples.mixed))[(plot.help[i]+1):plot.help[i+1]],1],
