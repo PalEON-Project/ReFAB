38d23d0fed214e737873b4609d8af57a5740dedf
ANN RAIHO <araiho@nd.edu>
2018-12-20 12:59:36 -0600

    adding u

--- Workflow_Code/utils/validation_args.R
+++ Workflow_Code/utils/validation_args.R
@@ -1,9 +1,12 @@
 
+#### These are important constants used in all calibrations and predictions
 
 Niters <- 50000
 bMax <- 228
 median_use <- 45
+u <- c(0,median_use,bMax)
 
+### For 10 fold cross validation only
 seed <- 5
 set.seed(seed)
 sets10 <- matrix(sample(x = 1:150,size = 150, replace = F),15,10)
