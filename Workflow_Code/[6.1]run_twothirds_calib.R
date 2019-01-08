a5bacd6ffc58fc38488feea260d56007ae0a2788
ANN RAIHO <araiho@nd.edu>
2019-01-08 07:13:23 -0500

    adding to run twothirds calibration with biomass uncertainty

--- Workflow_Code/[6.1]run_twothirds_calib.R
+++ Workflow_Code/[6.1]run_twothirds_calib.R
@@ -10,6 +10,7 @@ if (is.na(arg[1])) {
   runnum <- NA
 } else {
   group_rm <- as.numeric(arg[1])
+  runnum <- group_rm
 }
 
 library(nimble)
@@ -23,7 +24,26 @@ source(file.path('Workflow_Code','utils','validation_args.R')) #file with consta
 
 #### Setting up 10 fold cross validation
 Y.keep <- Y
-biomass.keep <- biomass
+
+#biomass.keep <- biomass
+
+#### Adds biomass data product uncertainty
+load('biomass_draws.Rdata')
+biomass.keep <- unlist(lapply(biomass_draws,function(x) x[runnum]))
+load("~/ReFAB/new_sites_rm.Rdata") # new 1/3
+load('~/ReFAB/sites_rm.Rdata') # old 1/3
+load("~/ReFAB/TF.Rdata") # old full
+
+bfull <- biomass.keep[TF]
+btt <- bfull[-sites_rm]
+b3 <- bfull[sites_rm]
+newbfull <- biomass.keep[-TF]
+newbtt <- newbfull[-sites_rm_new]
+newb3 <- newbfull[sites_rm_new]
+
+biomass1_3 <- c(b3,newb3)
+biomass2_3 <- c(btt,newbtt)
+biomass <- biomass2_3
 
 if(is.na(group_rm) | group_rm > 10){ #if true does full 2/3 fit
   Y.calib <- Y; Y.pred <- Y
--- Workflow_Code/utils/validation_args.R
+++ Workflow_Code/utils/validation_args.R
@@ -1,7 +1,7 @@
 
 #### These are important constants used in all calibrations and predictions
 
-Niters <- 50000
+Niters <- 10000
 bMax <- 228
 median_use <- 45
 u <- c(0,median_use,bMax)
@@ -10,3 +10,36 @@ u <- c(0,median_use,bMax)
 seed <- 5
 set.seed(seed)
 sets10 <- matrix(sample(x = 1:150,size = 150, replace = F),15,10)
+
+
+### function to draw credible intervals on a plot
+ciEnvelope <- function (x, ylo, yhi, ...) {
+  m <- rbind(x, ylo, yhi)
+  nas <- which(apply(is.na(m), 2, sum) > 0)
+  if (length(nas) > 0) {
+    sub.m <- list()
+    for (i in seq_along(nas)) {
+      if (i == 1) {
+        if (nas[i] > 1) {
+          sub.m[[i]] <- m[, 1:(nas[i] - 1)]
+        }
+      }
+      else {
+        if (nas[i] > (nas[i - 1] + 1)) {
+          sub.m[[i]] <- m[, (nas[i - 1] + 1):(nas[i] - 
+                                                1)]
+        }
+      }
+    }
+  }
+  else {
+    sub.m <- list(m = m)
+  }
+  for (i in seq_along(sub.m)) {
+    x <- sub.m[[i]]["x", ]
+    ylo <- sub.m[[i]]["ylo", ]
+    yhi <- sub.m[[i]]["yhi", ]
+    polygon(cbind(c(x, rev(x), x[1]), c(ylo, rev(yhi), ylo[1])), 
+            border = NA, ...)
+  }
+}
