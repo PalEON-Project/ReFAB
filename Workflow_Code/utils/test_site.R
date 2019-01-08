60cf99bf18f19fecd05b169ac443bcd5e1187ef7
ANN RAIHO <araiho@nd.edu>
2019-01-08 07:41:10 -0500

    printing site name

--- Workflow_Code/utils/test_site.R
+++ Workflow_Code/utils/test_site.R
@@ -1,12 +1,12 @@
 test_site <- function(x.meta.use){
   if(length(unique(x.meta.use$lat)) != 1) {
-    print('lats are not the same for this site name')
+    print(paste('lats are not the same for this site name',unique(x.meta.use$site.name)))
     stop()
   }
   if(length(unique(x.meta.use$long)) != 1) {
-    print('longs are not the same for this site name')
+    print(paste('longs are not the same for ',unique(x.meta.use$site.name)))
     stop()
   }
   
-  print('Site Passed Tests.')
+  print(paste(unique(x.meta.use$site.name),' Passed Tests.'))
 }
