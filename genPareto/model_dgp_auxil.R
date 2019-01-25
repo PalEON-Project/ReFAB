d9b1773d8310b55016bc7556f8bc6feb91b297c7
ANN RAIHO <araiho@nd.edu>
2019-01-25 17:13:20 -0500

    linexp needs to be in fix_b too

--- genPareto/model_dgp_auxil.R
+++ genPareto/model_dgp_auxil.R
@@ -146,8 +146,15 @@ pred_code_fix_b <- nimbleCode({
   # entire shape{1,2} matrices; this syntax accomplishes that
   # this is because b[t] updates have rows of shape{1,2} as dependents
   for(t in 1:T){
-    shape1[t,1:I] <- exp(Zb[t,1:5] %*% beta1[1:5,1:I])
-    shape2[t,1:I] <- exp(Zb[t,1:5] %*% beta2[1:5,1:I])
+    shape.hold1[t,1:I] <- Zb[t,1:5] %*% beta1[1:5,1:I]
+    shape.hold2[t,1:I] <- Zb[t,1:5] %*% beta2[1:5,1:I]
+  }
+  
+  for(t in 1:T){
+    for(i in 1:I){
+      shape1[t,i] <- linexp_vec(shape.hold1[t,i])
+      shape2[t,i] <- linexp_vec(shape.hold2[t,i])
+    }
   }
   
   for(j in 1:J){
