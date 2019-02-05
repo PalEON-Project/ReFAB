8a4bc78addf878d612cd0e20fa43c4a111dde9f9
ANN RAIHO <araiho@nd.edu>
2019-02-05 10:16:57 -0500

    making to match new data and structure

--- Workflow_Code/valid_iterative_taxa.R
+++ Workflow_Code/valid_iterative_taxa.R
@@ -6,8 +6,6 @@ if (is.na(arg[1])) {
   group_rm <- as.numeric(arg[1])
 }
 
-### after get.data
-
 library(nimble)
 library(splines)
 library(maps)
@@ -18,25 +16,15 @@ ciEnvelope <- function(x,ylo,yhi,...){
                                       ylo[1])), border = NA,...) 
 }
 
-# histogram of betas to see how it looks relative to prior
-# the prior might be too tight now. with linexp
-# rel to an sd of 5. 
-# might want a flat prior like => 1/400 precision
+load("twothirds_v2.0.Rdata")
 
-load("twothirds_v1.0.Rdata")
-
-Niters <- 5000
-bMax <- 150
+source(file.path('Workflow_Code','utils','validation_args.R')) #file with constants that should be constant between validation exercises
 
 #### Setting up taxa removal 
 Y.keep <- Y
 biomass.keep <- biomass
 Y.calib <- Y.pred <- Y[,-group_rm]
 biomass.calib <- biomass.pred <- biomass
-
-#### Making sure Z.knots and u are the same between calibration and validation
-#Z.knots = bs(biomass.calib, intercept=TRUE, knots = 30, Boundary.knots=c(0,bMax))
-u <- c(0,30,bMax) #c(rep(attr(Z.knots,"Boundary.knots")[1],1),attr(Z.knots,"knots"),rep(attr(Z.knots,"Boundary.knots")[2],1))
 
 source("Workflow_Code/utils/bs_nimble.R")
 Z.test <- matrix(NA,length(biomass.calib),5)
@@ -74,34 +62,36 @@ samples.pred <- validation_model(Y = Y.pred, Z.knots = Z.knots,
                                  Niters = Niters, bMax = bMax, group_rm = group_rm,
                                  outLik = outLik)
 
-
-samps.mat <- array(NA, dim = c(500,100,22))
-r.saved <- numeric(22)
-
-pdf('iterative.taxa.sens.pdf')
-par(mfrow=c(2,2))
-for(i in 1:22){ #order(r.saved)
-  load(paste0('~/Downloads/taxa.samps/samples.pred.group',i,'betaNA.Rdata'))
-  samps.mat[,,i] <- samples.pred
+if(FALSE){ ### To plot use the following after running on the server
+  samps.mat <- array(NA, dim = c(500,100,22))
+  r.saved <- numeric(22)
   
-  bio.median <- apply(samples.pred,2,FUN = quantile,.5)
-  plot(biomass, bio.median,
-       xlim=c(0,bMax), ylim=c(0,bMax), pch=19,
-       xlab="True Biomass", ylab="Predicted Mean Biomass",
-       main='New Biomass')
-  abline(a=0,b=1)
-  lm.mod <- lm(biomass ~ bio.median+0)
-  abline(lm.mod,lty=2)
-  mtext(paste("r-squared",summary(lm.mod)$r.squared))
-  
-  r.saved[i] <- summary(lm.mod)$r.squared
-  
-  arrows(x0 = biomass, y0 = apply(samples.pred,2,FUN = quantile,.05),
-         x1 = biomass, y1 = apply(samples.pred,2,FUN = quantile,.975),
-         code = 0, lwd=2)
-  
+  pdf('iterative.taxa.sens.pdf')
+  par(mfrow=c(2,2))
+  for(i in 1:22){ #order(r.saved)
+    load(paste0('~/Downloads/taxa.samps/samples.pred.group',i,'betaNA.Rdata'))
+    samps.mat[,,i] <- samples.pred
+    
+    bio.median <- apply(samples.pred,2,FUN = quantile,.5)
+    plot(biomass, bio.median,
+         xlim=c(0,bMax), ylim=c(0,bMax), pch=19,
+         xlab="True Biomass", ylab="Predicted Mean Biomass",
+         main='New Biomass')
+    abline(a=0,b=1)
+    lm.mod <- lm(biomass ~ bio.median+0)
+    abline(lm.mod,lty=2)
+    mtext(paste("r-squared",summary(lm.mod)$r.squared))
+    
+    r.saved[i] <- summary(lm.mod)$r.squared
+    
+    arrows(x0 = biomass, y0 = apply(samples.pred,2,FUN = quantile,.05),
+           x1 = biomass, y1 = apply(samples.pred,2,FUN = quantile,.975),
+           code = 0, lwd=2)
+    
+  }
+  dev.off()  
 }
-dev.off()
+
 
 
 
