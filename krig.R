b153e421a5c82a301d421858c1e1ebba38589c35
Ann Raiho <annraiho@cnr163-71.cnr.colostate.edu>
2020-01-29 13:29:38 -0700

    trying out some different covariance models

--- krig.R
+++ krig.R
@@ -2,6 +2,8 @@
 library(geoR)
 library(raster)
 library(maptools)
+library(ncdf4)
+library(maps)
 
 #### 
 ##### Get prediction coordiantes from PLS map
@@ -36,7 +38,6 @@ plot(x,y)
 #### Change from Albers to lat / lon
 #### 
 
-library(raster)
 albers.df = as.data.frame(cbind(x,y))
 colnames(albers.df) = c('x', 'y')
 
@@ -51,15 +52,18 @@ coors_dat <- as.matrix(data.frame(lat.lon))
 #### 
 
 refab <- read.csv('median_biomass_plus_meta.csv')
-y <- refab$X100_Years_BP
 
+library(spatstat)
+
+ch <- convexhull.xy(x= refab$lon,y=refab$lat)
 
 #### 
 #### Variogram
 ####  
 
-par(mfrow=c(3,3))
-for(tt in rev(seq(1,100,10))){
+pdf('refab_krig_exponential.pdf',height=15,width = 15)
+par(mfrow=c(3,3),mar=c(0,0,4,0))
+for(tt in rev(seq(10,90,10))){
 
 time_do <- colnames(refab)[2:101][tt]
 time_slice <- data.frame(refab[, c('lon', 'lat', time_do)],X=rep(1,77))
@@ -68,7 +72,8 @@ ut.gd <- as.geodata(time_slice, coords.col =
 
 ut.v = variog(ut.gd,
               trend =  ~ refab[, c('lon')] + refab[, c('lat')],
-              max.dist = max(dist(refab[, c('lon', 'lat')])) / 2)
+              max.dist = max(dist(refab[, c('lon', 'lat')]))/3)
+
 ut.wls = variofit(
   ut.v,
   ini = c(400, 6),
@@ -78,12 +83,12 @@ ut.wls = variofit(
   wei = "cressie"
 )
 
-#ut.wls
 
-if(FALSE){
-  par(mfrow=c(1,1))
-  plot(ut.v)
+if(TRUE){
+  #par(mfrow=c(1,1))
+  plot(ut.v,pch=19)
   lines(ut.wls) 
+  title('Semi-Variogram')
 }
 
 #### 
@@ -93,6 +98,7 @@ if(FALSE){
 ut.krig = krige.conv(
   ut.gd,
   locations = coors_dat,
+  borders=NULL,
   krige = krige.control(
     type.krige = "OK",
     obj.m = ut.wls
@@ -102,15 +108,32 @@ ut.krig = krige.conv(
 grid <- data.frame(coors_dat)
 grid$z <- ut.krig$predict
 
-breaks <- seq(0,400,5)
-data_binned <- cut(grid$z,breaks=breaks,labels=F)
-colors <- rev(terrain.colors(length(breaks)))
+breaks <- breaks <-  c(seq(0,50,10),seq(75,250,25),435)
+data_binned <- cut(grid$z,breaks=breaks,labels=F,include.lowest=F)
+colors <- rev(terrain.colors(length(breaks)-1))
 
 data_binned_pts <- cut(time_slice[,3],breaks=breaks,labels=F)
 
-plot(grid$x,grid$y,col=colors[data_binned],pch=19)
-points(time_slice$lon,time_slice$lat,pch=21,bg = colors[data_binned_pts])
+plot(grid$x,grid$y,col=adjustcolor(colors[data_binned],alpha.f = .5),pch=19)
+plot(ch,add=T)
+points(time_slice$lon,time_slice$lat,pch=21,bg = colors[data_binned_pts],cex=2)
 map('state',add=T)
+title(paste(tt*100,'Year BP Prediction'))
+legend('topright',legend=breaks,col=colors,pch=19)
+
+var_breaks <- seq(0,3000,500)
+var_data_binned <- cut(ut.krig$krige.var,breaks=var_breaks,labels=F)
+white_red <- colorRampPalette(c('white','bisque','red','darkorchid4'))
+var_colors <- (white_red(length(var_breaks)))
+
+
+plot(grid$x,grid$y,col=var_colors[var_data_binned],pch=19)
+plot(ch,add=T)
+points(time_slice$lon,time_slice$lat,pch=21,bg = 'black')
+map('state',add=T)
+legend('topright',legend=var_breaks,col=var_colors,pch=19)
+title('Variance')
+
 
 }
-
+dev.off()
